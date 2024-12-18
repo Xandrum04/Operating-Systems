65e4c5cee02a75a0b0ea5d3b48dfe0884a05707a
ArcaneTrixter99 <apostoliszk@gmail.com>
2024-12-19 01:59:05 +0200

    Update ipc_utils.h

--- askisi2/ipc_utils.h
+++ askisi2/ipc_utils.h
@@ -1,4 +1,3 @@
-// ipc_utils.h
 #ifndef IPC_UTILS_H
 #define IPC_UTILS_H
 
@@ -6,31 +5,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Συνάρτηση για τη δημιουργία σημαφόρου
+// ΔΗΜΙΟΥΡΓΙΑ ΣΗΜΑΦΟΡΟΥ
 HANDLE create_semaphore(const char *name, LONG initialCount, LONG maxCount) {
     HANDLE semaphore = CreateSemaphoreA(NULL, initialCount, maxCount, name);
     if (semaphore == NULL) {
-        fprintf(stderr, "Error: Unable to create semaphore. Error code: %lu\n", GetLastError());
+        fprintf(stderr, "Error: ΑΔΥΝΑΜΙΑ ΔΗΜΙΟΥΡΓΙΑΣ ΣΗΜΑΦΟΡΟΥ. Error code: %lu\n", GetLastError());
         exit(EXIT_FAILURE);
     }
     return semaphore;
 }
 
-// Συνάρτηση για να περιμένει μια διεργασία σε σημαφόρο
+// ΣΥΝΑΡΤΗΣΗ ΑΝΑΜΟΝΗΣ ΜΙΑΣ ΔΙΕΡΓΑΣΙΑΣ ΣΕ ΣΗΜΑΦΟΡΟ
 void wait_semaphore(HANDLE semaphore) {
     DWORD waitResult = WaitForSingleObject(semaphore, INFINITE);
     if (waitResult != WAIT_OBJECT_0) {
-        fprintf(stderr, "Error: Failed to wait on semaphore. Error code: %lu\n", GetLastError());
+        fprintf(stderr, "Error: ΑΔΥΝΑΜΙΑ ΝΑ ΠΕΡΙΜΕΝΟΥΜΕ ΤΟ ΣΗΜΑΦΟΡΟ. Error code: %lu\n", GetLastError());
         exit(EXIT_FAILURE);
     }
 }
 
-// Συνάρτηση για να απελευθερώσει μια θέση στον σημαφόρο
+// ΣΥΝΑΡΤΗΣΗ ΓΙΑ ΑΠΕΛΕΥΘΕΡΩΣΗ ΘΕΣΗΣ
 void release_semaphore(HANDLE semaphore) {
     if (!ReleaseSemaphore(semaphore, 1, NULL)) {
-        fprintf(stderr, "Error: Failed to release semaphore. Error code: %lu\n", GetLastError());
+        fprintf(stderr, "Error: ΑΔΥΝΑΜΙΑ ΑΠΕΛΕΥΘΕΡΩΣΗΣ ΘΕΣΗΣ. Error code: %lu\n", GetLastError());
         exit(EXIT_FAILURE);
     }
 }
 
-#endif // IPC_UTILS_H
+#endif
