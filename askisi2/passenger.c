5911078961fc2ec8768a8a2e5db9bb6b66630ca9
ArcaneTrixter99 <apostoliszk@gmail.com>
2024-12-19 01:03:36 +0200

    Update passenger.c

--- askisi2/passenger.c
+++ askisi2/passenger.c
@@ -1,8 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
+// passenger.c
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+#include <stdio.h>
+#include "ipc_utils.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <semaphore_name>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *semaphore_name = argv[1];
+
+    // Άνοιγμα σημαφόρου
+    HANDLE semaphore = OpenSemaphoreA(SEMAPHORE_ALL_ACCESS, FALSE, semaphore_name);
+    if (semaphore == NULL) {
+        fprintf(stderr, "Failed to open semaphore: %lu\n", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    printf("Passenger attempting to board...\n");
+
+    // Αναμονή για θέση στη λέμβο
+    wait_semaphore(semaphore);
+
+    printf("Passenger boarded successfully!\n");
+
+    // Προσομοίωση χρόνου παραμονής στη λέμβο
+    Sleep(2000);
+
+    printf("Passenger disembarking and freeing up a spot.\n");
+
+    // Απελευθέρωση θέσης
+    release_semaphore(semaphore);
+
+    // Κλείσιμο σημαφόρου
+    CloseHandle(semaphore);
+
+    return EXIT_SUCCESS;
 }
